0d1391441cefead26ecbac4c5cdb17a80b1c7788
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-05-24 12:20:23 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -6,43 +6,129 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      commonAttrs = { doCheck ? false, sanitizer ? "", sanitizerFlags ? "" }: {
+        src = ./.;
+        checkInputs = with pkgs; [ gtest ];
+        propagatedBuildInputs = [ abc-verifier ];
+        installPhase = ''
+          make install PREFIX=$out ABCEXTERNAL=yosys-abc
+          ln -s ${abc-verifier}/bin/abc $out/bin/yosys-abc
+        '';
+        meta = with pkgs.lib; {
+          description = "Yosys Open SYnthesis Suite";
+          homepage = "https://yosyshq.net/yosys/";
+          license = licenses.isc;
+          maintainers = with maintainers; [ ];
         };
-        # TODO: don't override src when ./abc is empty
-        # which happens when the command used is `nix build` and not `nix build ?submodules=1`
-        abc-verifier = pkgs.abc-verifier.overrideAttrs(x: y: {src = ./abc;});
-        yosys = pkgs.clangStdenv.mkDerivation {
-          name = "yosys";
-          src = ./. ;
-          buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang zlib git pkg-configUpstream ];
-          checkInputs = with pkgs; [ gtest ];
-          propagatedBuildInputs = [ abc-verifier ];
-          preConfigure = "make config-clang";
-          checkTarget = "test";
-          installPhase = ''
-            make install PREFIX=$out ABCEXTERNAL=yosys-abc
-            ln -s ${abc-verifier}/bin/abc $out/bin/yosys-abc
-          '';
-					buildPhase = ''
-            make -j$(nproc) ABCEXTERNAL=yosys-abc
-          '';
-          meta = with pkgs.lib; {
-            description = "Yosys Open SYnthesis Suite";
-            homepage = "https://yosyshq.net/yosys/";
-            license = licenses.isc;
-            maintainers = with maintainers; [ ];
-          };
-        };
-      in {
-        packages.default = yosys;
-        defaultPackage = yosys;
-        devShell = pkgs.mkShell {
+        doCheck = doCheck;
+        dontStrip = true;  # Ensure that debugging symbols are not stripped
+      };
+
+      abc-verifier = pkgs.abc-verifier.overrideAttrs (x: y: { src = ./abc; });
+
+      yosys = pkgs.clangStdenv.mkDerivation (commonAttrs {
+        doCheck = false;
+      } // {
+        name = "yosys";
+        buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang zlib git pkg-configUpstream gcc ];
+        preConfigure = "make config-clang";
+        checkTarget = "test";
+        buildPhase = ''
+          export CXXFLAGS="$CXXFLAGS -g"  # Enable debug symbols
+          export LDFLAGS="-L${pkgs.gcc.libc}/lib"
+          make -j$(nproc) ABCEXTERNAL=yosys-abc
+        '';
+      });
+
+      gcc-yosys = pkgs.gccStdenv.mkDerivation (commonAttrs {
+        doCheck = false;
+      } // {
+        name = "gcc-yosys";
+        buildInputs = with pkgs; [ gcc bison flex libffi tcl readline python3 llvmPackages.libcxx zlib git pkg-configUpstream ];
+        preConfigure = "make config-gcc";
+        checkTarget = "test";
+        buildPhase = ''
+          export CXXFLAGS="$CXXFLAGS -g"  # Enable debug symbols
+          make -j$(nproc) ABCEXTERNAL=yosys-abc
+        '';
+      });
+
+      yosysWithSanitizers = sanitizer: pkgs.clangStdenv.mkDerivation (commonAttrs {
+        doCheck = true;
+        sanitizer = sanitizer;
+        sanitizerFlags = if sanitizer == "memory" then "-fsanitize-memory-track-origins=2" else "";
+      } // {
+        name = "yosys-with-${sanitizer}";
+        buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang llvmPackages.libstdcxxClang zlib git pkg-configUpstream ];
+        preConfigure = "make config-clang";
+        checkTarget = "test";
+        buildPhase = ''
+          export CXXFLAGS="$CXXFLAGS -g -fsanitize=${sanitizer} ${if sanitizer == "memory" then "-fsanitize-memory-track-origins=2" else ""}"  # Enable debug symbols and sanitizer flags
+          export LDFLAGS="-L${pkgs.gcc.libc}/lib"
+          make -j$(nproc) ABCEXTERNAL=yosys-abc SANITIZER=${sanitizer}
+        '';
+        checkPhase = ''
+          export CXXFLAGS="$CXXFLAGS -g -fsanitize=${sanitizer} ${if sanitizer == "memory" then "-fsanitize-memory-track-origins=2" else ""}"  # Enable debug symbols and sanitizer flags
+          export LDFLAGS="-L${pkgs.gcc.libc}/lib"
+          make test SANITIZER=${sanitizer}
+        '';
+        doCheck = false;
+      });
+
+      yosysWithMultipleSanitizers = pkgs.clangStdenv.mkDerivation (commonAttrs {
+        doCheck = true;
+      } // {
+        name = "yosys-with-multiple-sanitizers";
+        buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang zlib git pkg-configUpstream gcc ];
+        preConfigure = "make config-clang";
+        checkTarget = "test";
+        buildPhase = ''
+          export CXXFLAGS="$CXXFLAGS -g -fsanitize=address,undefined"  # Enable debug symbols and multiple sanitizers
+          export LDFLAGS="-L${pkgs.gcc.libc}/lib"
+          make -j$(nproc) ABCEXTERNAL=yosys-abc SANITIZER="address,undefined"
+        '';
+        checkPhase = ''
+          export CXXFLAGS="$CXXFLAGS -g -fsanitize=address,undefined"  # Enable debug symbols and multiple sanitizers
+          export LDFLAGS="-L${pkgs.gcc.libc}/lib"
+          make test SANITIZER="address,undefined"
+        '';
+      });
+
+      sanitizers = [ "address" "memory" "undefined" "cfi" ];
+      sanitizerPackages = map (sanitizer: {
+        name = "yosys-with-${sanitizer}";
+        value = yosysWithSanitizers sanitizer;
+      }) sanitizers;
+
+    in {
+      packages = flake-utils.lib.flattenTree (builtins.listToAttrs ([
+        { name = "yosys"; value = yosys; }
+        { name = "gcc-yosys"; value = gcc-yosys; }
+        { name = "yosys-with-multiple-sanitizers"; value = yosysWithMultipleSanitizers; }
+      ] ++ sanitizerPackages));
+      defaultPackage = yosys;
+      devShells = {
+        default = pkgs.mkShell {
           buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang zlib git gtest abc-verifier ];
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
         };
-      }
-    );
+      };
+      
+      checks = {
+        makeTest = pkgs.runCommand "make-test" {
+          buildInputs = [ pkgs.makeWrapper ];
+        } ''
+          cp -r ${self}/* .
+          export PATH=$PATH:${pkgs.make}/bin
+          make test
+        '';
+      };
+    }
+  );
 }
