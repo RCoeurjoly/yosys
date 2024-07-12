717015837fabf6a98b260812e650de863d104914
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-07-12 12:49:14 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -41,7 +41,8 @@
         packages.default = yosys;
         defaultPackage = yosys;
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang zlib git gtest abc-verifier ghdl yosys-ghdl z3 poetry gtkwave ];
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+          buildInputs = with pkgs; [ clang bison flex libffi tcl readline python3 llvmPackages.libcxxClang zlib git gtest abc-verifier ghdl yosys-ghdl z3 poetry gtkwave lcov ];
         };
       }
     );
